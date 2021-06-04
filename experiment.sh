e071fee5b85bee9670f51fd7e60ff2852cb1986e
Igor Cavalcanti <igor.lima@luizalabs.com>
2021-06-04 14:27:39 -0300

    update experiments script

--- experiment.sh
+++ experiment.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-threads=(1 2 4 8)
-instances="BA CE GOeDF MG MS MT PA PE PI PR RJ RS SC SP TO"
+threads="8"
+instances="TO MS PE RJ ES"
 
 for instance in ${instances[@]}; do
   for num_thread in ${threads[@]}; do
-    out_file="results/${instance}_${num_thread}.out"
+    out_file="results_igor/${instance}_${num_thread}.out"
     echo $out_file
+    echo "pop: 20; pe: 0.3; pm: 0.2; generations: 100; c1: 20; c2: 20; c3: 10" >> $out_file
     echo "Instance;FO;Time" >> $out_file
     for i in $(seq 1 10); do
-      ./fast-tclp-brkga "instances/${instance}.txt" 20 10 5 $i $num_thread >> $out_file
+      ./tclp-brkga "instances/${instance}.txt" 20 20 10 $i $num_thread >> $out_file
     done
   done
 done
